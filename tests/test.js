1e4db90fbc4af1708f68ad3e2f1787666433766d
Russ Carey <russellwcarey@gmail.com>
2020-08-02 22:52:47 -0600

    update due to VSCode acting weird

---
+++ tests/test.js
@@ -0,0 +1,17 @@
+var google = {}
+module.exports = {
+  beforeEach: browser => {
+    google = browser.page.googlePage()
+    google
+    .navigate()
+  },
+  after: browser => {
+    google
+    .end()
+  },
+  'Your test here': browser => {
+    google.
+    .setValue('@searchBar', ['Kittens', browser.Keys.ENTER])
+    .verify.containsText('@results', 'Kittens')
+  }
+}
